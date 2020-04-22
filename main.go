5dddaf1e3ab68312305b9020eb37b29c1de4975f
sasatake <t06418as@gmail.com>
2020-04-22 05:51:19 +0000

    add template handler

--- main.go
+++ main.go
@@ -3,22 +3,26 @@ package main
 import (
 	"log"
 	"net/http"
+	"path/filepath"
+	"sync"
+	"text/template"
 )
 
+type templateHandler struct {
+	once     sync.Once
+	filename string
+	templ    *template.Template
+}
+
+func (t *templateHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
+	t.once.Do(func() {
+		t.templ = template.Must(template.ParseFiles(filepath.Join("templates", t.filename)))
+	})
+	t.templ.Execute(w, nil)
+}
+
 func main() {
-	myfunction := func(w http.ResponseWriter, r *http.Request) {
-		w.Write([]byte(`
-		<html>
-			<head>
-				<title>チャット</title>
-			</head>
-			<body>
-				<h1>チャットしましょう</h1>
-			</body>
-		</html>
-		`))
-	}
-	http.HandleFunc("/", myfunction)
+	http.Handle("/", &templateHandler{filename: "main.html"})
 
 	if err := http.ListenAndServe(":8080", nil); err != nil {
 		log.Fatal("ListenAndServe:", err)
